bea40eabb80e8395ef6372d9a460d07a3ca317de
JomarMonreal <jpmonreal@up.edu.ph>
2024-05-10 04:38:21 +0800

    feat: finished non functional donor home page

--- lib/screens/donor_home_page.dart
+++ lib/screens/donor_home_page.dart
@@ -1,8 +1,11 @@
 import 'package:elbi_donation_system/components/main_drawer.dart';
+import 'package:elbi_donation_system/dummy_data/dummy_orgs.dart';
 import 'package:elbi_donation_system/models/route_model.dart';
 import 'package:elbi_donation_system/screens/log_in_page.dart';
 import 'package:elbi_donation_system/screens/org_home_page.dart';
 import 'package:flutter/material.dart';
+
+import '../components/page_cover.dart';
 
 class DonorHomePage extends StatefulWidget {
   const DonorHomePage({super.key});
@@ -13,22 +16,85 @@ class DonorHomePage extends StatefulWidget {
 
 class _DonorHomePageState extends State<DonorHomePage> {
   final _formKey = GlobalKey<FormState>();
-  RouteModel orgHomepage =
-      RouteModel("Home", "/org-home-page", const OrgHomePage());
+
+  final organizations = dummyOrgs;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: MainDrawer(routes: [
-        orgHomepage,
+        RouteModel("Home", "/donor-home-page", const OrgHomePage()),
         RouteModel("Logout", "/", const LoginPage()),
       ]),
       appBar: AppBar(
         title: const Text("Donor Home Page"),
       ),
-      body: Form(
-        key: _formKey,
-        child: const Center(child: Text("Insert something here")),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const PageCover(),
+              const Padding(
+                padding: EdgeInsets.all(30),
+                child: Center(
+                  child: Text(
+                    "Want to Donate Something?",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 200,
+                  height: 90,
+                  child: Text(
+                    "Here are the available organizations you can contact for donation:",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              ListView.builder(
+                  reverse: true,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: organizations.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Center(
+                              child: Flexible(
+                                  child: Image.network(
+                                organizations[index].imageUrl,
+                                fit: BoxFit.cover,
+                                width: MediaQuery.of(context).size.width / 2,
+                              )),
+                            ),
+                            Center(child: Text(organizations[index].name)),
+                            Center(
+                                child: Text(organizations[index].description)),
+                            Center(
+                              child: TextButton.icon(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.handshake_rounded),
+                                  label: const Text("Donate")),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  })
+            ],
+          ),
+        ),
       ),
     );
   }
