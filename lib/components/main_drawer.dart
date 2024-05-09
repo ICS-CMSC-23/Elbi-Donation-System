828df24b2e50d3a612ddf04678129f44c610055a
JomarMonreal <jpmonreal@up.edu.ph>
2024-05-09 19:06:01 +0800

    feat: drawer and routers added

---
+++ lib/components/main_drawer.dart
@@ -0,0 +1,39 @@
+import 'package:elbi_donation_system/models/route_model.dart';
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatefulWidget {
+  final List<RouteModel> routes;
+  const MainDrawer({super.key, required this.routes});
+
+  @override
+  State<MainDrawer> createState() => _MainDrawerState();
+}
+
+class _MainDrawerState extends State<MainDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            child: Text(
+              "Elbi Donation System",
+            ),
+          ),
+          ListView.builder(
+              reverse: true,
+              shrinkWrap: true,
+              itemCount: widget.routes.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                    title: Text(widget.routes[index].name),
+                    onTap: () {
+                      Navigator.pushNamed(context, widget.routes[index].path);
+                    });
+              })
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/models/route_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RouteModel {
+  String name;
+  String path;
+  Widget page;
+
+  RouteModel(this.name, this.path, this.page);
+}
