69b95020a6bcbc111f98bae008c6f90d3bb828c7
JomarMonreal <jpmonreal@up.edu.ph>
2024-05-14 22:13:13 +0800

    chore: phots in dummy donations

--- lib/dummy_data/dummy_donations.dart
+++ lib/dummy_data/dummy_donations.dart
@@ -10,7 +10,10 @@ List<Donation> dummyDonations = [
     isForPickup: false,
     weightInKg: 48.0,
     dateTime: DateTime.fromMicrosecondsSinceEpoch(1640979000000000),
-    photos: ["photo_929.jpg", "photo_116.jpg"],
+    photos: [
+      "https://ae01.alicdn.com/kf/HTB1ykfvk1OSBuNjy0Fdq6zDnVXa1/Toddler-Baby-Boy-Summer-Clothes-Newborn-Plaid-Gentlemen-Clothing-Set-2pcs-Formal-Clothing-Infant-Clothes-Wear.jpg",
+      "photo_116.jpg"
+    ],
     addresses: ["address_52"],
     contactNo: "123-456-0024",
     status: "Confirmed",
@@ -24,7 +27,10 @@ List<Donation> dummyDonations = [
     isForPickup: false,
     weightInKg: 47.0,
     dateTime: DateTime.fromMicrosecondsSinceEpoch(1640979000050000),
-    photos: ["photo_629.jpg"],
+    photos: [
+      "https://media.gettyimages.com/id/51101394/photo/potatoes-photo-taken-august-1999.jpg?b=1&s=594x594&w=0&k=20&c=pCSenjlQvEe3-qbpWXtxo03A6y1R6D37gbSx5wAp6c4=",
+      "photo_441.jpg"
+    ],
     addresses: ["address_20"],
     contactNo: "123-456-0166",
     status: "Canceled",
@@ -38,7 +44,10 @@ List<Donation> dummyDonations = [
     isForPickup: false,
     weightInKg: 38.0,
     dateTime: DateTime.fromMicrosecondsSinceEpoch(1640979007000000),
-    photos: ["photo_116.jpg", "photo_441.jpg"],
+    photos: [
+      "https://media.gettyimages.com/id/51101394/photo/potatoes-photo-taken-august-1999.jpg?b=1&s=594x594&w=0&k=20&c=pCSenjlQvEe3-qbpWXtxo03A6y1R6D37gbSx5wAp6c4=",
+      "photo_441.jpg"
+    ],
     addresses: ["address_23", "address_99"],
     contactNo: "123-456-0264",
     status: "Completed",
@@ -52,7 +61,10 @@ List<Donation> dummyDonations = [
     isForPickup: false,
     weightInKg: 18.0,
     dateTime: DateTime.fromMicrosecondsSinceEpoch(1640979490000000),
-    photos: ["photo_451.jpg", " photo_459.jpg"],
+    photos: [
+      "https://media.gettyimages.com/id/51101394/photo/potatoes-photo-taken-august-1999.jpg?b=1&s=594x594&w=0&k=20&c=pCSenjlQvEe3-qbpWXtxo03A6y1R6D37gbSx5wAp6c4=",
+      " photo_459.jpg"
+    ],
     addresses: ["address_58", "address_17"],
     contactNo: "123-456-0856",
     status: "Pending",
@@ -66,7 +78,10 @@ List<Donation> dummyDonations = [
     isForPickup: true,
     weightInKg: 3.0,
     dateTime: DateTime.fromMicrosecondsSinceEpoch(1640979000340000),
-    photos: [],
+    photos: [
+      "https://ae01.alicdn.com/kf/HTB1ykfvk1OSBuNjy0Fdq6zDnVXa1/Toddler-Baby-Boy-Summer-Clothes-Newborn-Plaid-Gentlemen-Clothing-Set-2pcs-Formal-Clothing-Infant-Clothes-Wear.jpg",
+      "photo_116.jpg"
+    ],
     addresses: ["address_36"],
     contactNo: "123-456-0084",
     status: "Canceled",
@@ -80,7 +95,10 @@ List<Donation> dummyDonations = [
     isForPickup: true,
     weightInKg: 36.0,
     dateTime: DateTime.fromMicrosecondsSinceEpoch(1640979000056000),
-    photos: [],
+    photos: [
+      "https://ae01.alicdn.com/kf/HTB1ykfvk1OSBuNjy0Fdq6zDnVXa1/Toddler-Baby-Boy-Summer-Clothes-Newborn-Plaid-Gentlemen-Clothing-Set-2pcs-Formal-Clothing-Infant-Clothes-Wear.jpg",
+      "photo_116.jpg"
+    ],
     addresses: ["address_33"],
     contactNo: "123-456-0283",
     status: "Confirmed",
@@ -94,7 +112,10 @@ List<Donation> dummyDonations = [
     isForPickup: true,
     weightInKg: 31.0,
     dateTime: DateTime.fromMicrosecondsSinceEpoch(1640979000123000),
-    photos: [],
+    photos: [
+      "https://ae01.alicdn.com/kf/HTB1ykfvk1OSBuNjy0Fdq6zDnVXa1/Toddler-Baby-Boy-Summer-Clothes-Newborn-Plaid-Gentlemen-Clothing-Set-2pcs-Formal-Clothing-Infant-Clothes-Wear.jpg",
+      "photo_116.jpg"
+    ],
     addresses: ["address_25", "address_12"],
     contactNo: "123-456-0246",
     status: "Canceled",
@@ -108,7 +129,10 @@ List<Donation> dummyDonations = [
     isForPickup: true,
     weightInKg: 36.0,
     dateTime: DateTime.fromMicrosecondsSinceEpoch(1640979000567000),
-    photos: [],
+    photos: [
+      "https://ae01.alicdn.com/kf/HTB1ykfvk1OSBuNjy0Fdq6zDnVXa1/Toddler-Baby-Boy-Summer-Clothes-Newborn-Plaid-Gentlemen-Clothing-Set-2pcs-Formal-Clothing-Infant-Clothes-Wear.jpg",
+      "photo_116.jpg"
+    ],
     addresses: ["address_13", "address_36"],
     contactNo: "123-456-0718",
     status: "Confirmed",
@@ -122,7 +146,10 @@ List<Donation> dummyDonations = [
     isForPickup: false,
     weightInKg: 13.0,
     dateTime: DateTime.fromMicrosecondsSinceEpoch(1640979000634000),
-    photos: [],
+    photos: [
+      "https://ae01.alicdn.com/kf/HTB1ykfvk1OSBuNjy0Fdq6zDnVXa1/Toddler-Baby-Boy-Summer-Clothes-Newborn-Plaid-Gentlemen-Clothing-Set-2pcs-Formal-Clothing-Infant-Clothes-Wear.jpg",
+      "photo_116.jpg"
+    ],
     addresses: ["address_89"],
     contactNo: "123-456-0608",
     status: "Confirmed",
@@ -136,7 +163,10 @@ List<Donation> dummyDonations = [
     isForPickup: false,
     weightInKg: 24.0,
     dateTime: DateTime.fromMicrosecondsSinceEpoch(1640979009900000),
-    photos: [],
+    photos: [
+      "https://ae01.alicdn.com/kf/HTB1ykfvk1OSBuNjy0Fdq6zDnVXa1/Toddler-Baby-Boy-Summer-Clothes-Newborn-Plaid-Gentlemen-Clothing-Set-2pcs-Formal-Clothing-Infant-Clothes-Wear.jpg",
+      "photo_116.jpg"
+    ],
     addresses: ["address_55", "address_23"],
     contactNo: "123-456-0422",
     status: "Scheduled for Pickup",
