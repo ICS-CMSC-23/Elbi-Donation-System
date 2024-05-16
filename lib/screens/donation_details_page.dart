eb28e36699fa87270c436d793e67a36cc258a5d5
JomarMonreal <jpmonreal@up.edu.ph>
2024-05-16 21:50:21 +0800

    chore: reordered information

--- lib/screens/donation_details_page.dart
+++ lib/screens/donation_details_page.dart
@@ -5,6 +5,7 @@ import 'package:elbi_donation_system/components/title_detail_list.dart';
 import 'package:elbi_donation_system/dummy_data/dummy_donations.dart';
 import 'package:elbi_donation_system/models/donation_model.dart';
 import 'package:elbi_donation_system/providers/donation_list_provider.dart';
+import 'package:elbi_donation_system/providers/user_list_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -27,9 +28,11 @@ class DonationDetails extends StatefulWidget {
 class _DonationDetailsState extends State<DonationDetails> {
   @override
   Widget build(BuildContext context) {
-    Donation dummyDonation =
-        context.watch<DonationListProvider>().currentDonation;
-    String userType = "donor"; //donor, organization, admin
+    Donation donation = context.watch<DonationListProvider>().currentDonation;
+    String userType = context
+        .watch<UserListProvider>()
+        .currentUser
+        .role; //donor, organization, admin
     Row actionButtons;
     if (userType == "donor") {
       actionButtons = Row(
@@ -81,12 +84,15 @@ class _DonationDetailsState extends State<DonationDetails> {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               HeaderWithPic(
-                  imageUrl: dummyDonation.photos![0],
-                  title: dummyDonation.category,
-                  subtitle: dummyDonation.status,
-                  description: dummyDonation.description),
+                  imageUrl: donation.photos![0],
+                  title: donation.category,
+                  subtitle: donation.status,
+                  description:
+                      "Donor: ${context.read<UserListProvider>().userList.firstWhere((user) => user.id == donation.donorId).name}"),
+              TitleDetail(title: "Description", detail: donation.description),
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Expanded(
                     flex: 1,
@@ -94,49 +100,44 @@ class _DonationDetailsState extends State<DonationDetails> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         TitleDetail(
-                            title: "Donor", detail: dummyDonation.donorId),
+                            title: "Weight (in kg)",
+                            detail: donation.weightInKg.toString()),
                         TitleDetail(
                             title: "Date",
                             detail: DateFormat("yy-MM-dd")
-                                .format(dummyDonation.dateTime)),
+                                .format(donation.dateTime)),
                       ],
                     ),
                   ),
-                  Expanded(
+                  const Expanded(
                     flex: 1,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TitleDetail(
-                            title: "Weight (in kg)",
-                            detail: dummyDonation.weightInKg.toString()),
-                        const TitleDetail(
-                          title: "Contact Number",
-                          detail: "09762946252",
-                        ),
-                      ],
+                    child: TitleDetail(
+                      title: "Contact Number",
+                      detail: "09762946252",
                     ),
                   ),
                 ],
               ),
-              TitleDetailList(
-                  title: "Address", detailList: dummyDonation.addresses),
-              const Text(
-                "Photo Details",
-                style: TextStyle(fontWeight: FontWeight.bold),
+              TitleDetailList(title: "Address", detailList: donation.addresses),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  "Photo Details",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
               ),
               GridView.builder(
                   shrinkWrap: true,
                   physics: const NeverScrollableScrollPhysics(),
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 3),
-                  itemCount: dummyDonation.photos?.length,
+                  itemCount: donation.photos?.length,
                   itemBuilder: (BuildContext context, int index) {
                     return Padding(
                         padding: const EdgeInsets.all(5.00),
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(10.0),
-                          child: Image.network(dummyDonation.photos![index],
+                          child: Image.network(donation.photos![index],
                               fit: BoxFit.cover),
                         ));
                   }),
