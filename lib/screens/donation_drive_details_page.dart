009584d489c9c41623de8c3429a9ae1526423fc0
JomarMonreal <jpmonreal@up.edu.ph>
2024-05-16 22:12:33 +0800

    chore: removed single child scroll view

--- lib/screens/donation_drive_details_page.dart
+++ lib/screens/donation_drive_details_page.dart
@@ -1,13 +1,16 @@
 import 'package:elbi_donation_system/components/header_with_pic.dart';
 import 'package:elbi_donation_system/components/main_drawer.dart';
+import 'package:elbi_donation_system/components/rounded_image.dart';
 import 'package:elbi_donation_system/components/title_detail.dart';
 import 'package:elbi_donation_system/models/donation_drive_model.dart';
 import 'package:elbi_donation_system/providers/donation_drive_list_provider.dart';
+import 'package:elbi_donation_system/providers/donation_list_provider.dart';
 import 'package:elbi_donation_system/providers/user_list_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+import '../models/donation_model.dart';
 import '../models/route_model.dart';
 
 class DonationDriveDetails extends StatefulWidget {
@@ -32,6 +35,11 @@ class _DonationDriveDetailsState extends State<DonationDriveDetails> {
   Widget build(BuildContext context) {
     DonationDrive donationDrive =
         context.watch<DonationDriveListProvider>().currentDonationDrive;
+    List<Donation> donations = context
+        .read<DonationListProvider>()
+        .donationList
+        .where((donation) => donation.donationDriveId == donationDrive.id)
+        .toList();
     String userType = context
         .watch<UserListProvider>()
         .currentUser
@@ -87,66 +95,96 @@ class _DonationDriveDetailsState extends State<DonationDriveDetails> {
       appBar: AppBar(
         title: const Text("Donation Drive Details"),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              HeaderWithPic(
-                  imageUrl: donationDrive.photos![0],
-                  title: donationDrive.name,
-                  subtitle: donationDrive.isCompleted
-                      ? "Event Finished"
-                      : "Event Ongoing",
-                  description:
-                      "Organization: ${context.read<UserListProvider>().userList.firstWhere((user) => user.id == donationDrive.organizationId).name}"),
-              TitleDetail(
-                title: "Description",
-                detail: donationDrive.description,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: TitleDetail(
-                        title: "Start Date",
-                        detail: DateFormat("yy-MM-dd")
-                            .format(donationDrive.startDate)),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: TitleDetail(
-                        title: "End Date",
-                        detail: DateFormat("yy-MM-dd")
-                            .format(donationDrive.endDate)),
-                  ),
-                ],
-              ),
-              const Text(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            HeaderWithPic(
+                imageUrl: donationDrive.photos![0],
+                title: donationDrive.name,
+                subtitle: donationDrive.isCompleted
+                    ? "Event Finished"
+                    : "Event Ongoing",
+                description:
+                    "Organization: ${context.read<UserListProvider>().userList.firstWhere((user) => user.id == donationDrive.organizationId).name}"),
+            TitleDetail(
+              title: "Description",
+              detail: donationDrive.description,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: TitleDetail(
+                      title: "Start Date",
+                      detail: DateFormat("yy-MM-dd")
+                          .format(donationDrive.startDate)),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TitleDetail(
+                      title: "End Date",
+                      detail:
+                          DateFormat("yy-MM-dd").format(donationDrive.endDate)),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
                 "Photo Details",
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
-              GridView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3),
-                  itemCount: donationDrive.photos?.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                        padding: const EdgeInsets.all(5.00),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10.0),
-                          child: Image.network(donationDrive.photos![index],
-                              fit: BoxFit.cover),
-                        ));
+            ),
+            GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3),
+                itemCount: donationDrive.photos?.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                      padding: const EdgeInsets.all(5.00),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.network(donationDrive.photos![index],
+                            fit: BoxFit.cover),
+                      ));
+                }),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                "Donations",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: donations.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: RoundedImage(
+                          source: donations[index].photos![0], size: 50),
+                      title: Text(donations[index].category),
+                      subtitle: Text(donations[index].description),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.handshake),
+                        onPressed: () {
+                          context
+                              .read<DonationListProvider>()
+                              .setCurrentDonation(
+                                  donations[index].id ?? "donation_0");
+                          Navigator.pushNamed(context, "/donation-details");
+                        },
+                      ),
+                    );
                   }),
-              actionButtons
-            ],
-          ),
+            ),
+            actionButtons,
+          ],
         ),
       ),
     );
