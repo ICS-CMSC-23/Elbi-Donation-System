c8d5286eea6293308d1cc4e4124375bab49d60a9
JomarMonreal <jpmonreal@up.edu.ph>
2024-05-17 16:19:11 +0800

    feat: working sign up page

--- lib/screens/sign_up_page.dart
+++ lib/screens/sign_up_page.dart
@@ -171,208 +171,211 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text("Sign Up"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ChoiceChip(
-                    label: const Text('Donor'),
-                    selected: isDonor,
-                    onSelected: (selected) {
-                      setState(() {
-                        isDonor = true;
-                      });
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ChoiceChip(
+                      label: const Text('Donor'),
+                      selected: isDonor,
+                      onSelected: (selected) {
+                        setState(() {
+                          isDonor = true;
+                        });
+                      },
+                    ),
+                    const SizedBox(width: 8),
+                    ChoiceChip(
+                      label: const Text('Organization'),
+                      selected: !isDonor,
+                      onSelected: (selected) {
+                        setState(() {
+                          isDonor = false;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                if (isDonor) ...[
+                  TextFormField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your name';
+                      }
+                      return null;
                     },
                   ),
-                  const SizedBox(width: 8),
-                  ChoiceChip(
-                    label: const Text('Organization'),
-                    selected: !isDonor,
-                    onSelected: (selected) {
-                      setState(() {
-                        isDonor = false;
-                      });
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: userNameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                      prefixIcon: Icon(Icons.person_outline),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your username';
+                      }
+                      return null;
                     },
                   ),
+                ] else ...[
+                  TextFormField(
+                    controller: orgNameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Name of Organization',
+                      prefixIcon: Icon(Icons.business),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter the name of your organization';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  GestureDetector(
+                    onTap: _pickImage,
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      color: Colors.grey[300],
+                      child: _image != null
+                          ? Image.file(_image!, fit: BoxFit.cover)
+                          : const Icon(Icons.camera_alt, color: Colors.grey),
+                    ),
+                  ),
+                  // const SizedBox(height: 16),
+                  // TextFormField(
+                  //   controller: proofController,
+                  //   decoration: const InputDecoration(
+                  //     labelText: 'Proof of Legitimacy',
+                  //     prefixIcon: Icon(Icons.verified),
+                  //   ),
+                  //   validator: (value) {
+                  //     if (value!.isEmpty) {
+                  //       return 'Please provide proof of legitimacy';
+                  //     }
+                  //     return null;
+                  //   },
+                  // ),
                 ],
-              ),
-              const SizedBox(height: 16),
-              if (isDonor) ...[
+                const SizedBox(height: 16),
                 TextFormField(
-                  controller: nameController,
+                  controller: passwordController,
                   decoration: const InputDecoration(
-                    labelText: 'Name',
-                    prefixIcon: Icon(Icons.person),
+                    labelText: 'Password',
+                    prefixIcon: Icon(Icons.lock),
                   ),
+                  obscureText: true,
                   validator: (value) {
                     if (value!.isEmpty) {
-                      return 'Please enter your name';
+                      return 'Please enter your password';
                     }
                     return null;
                   },
                 ),
                 const SizedBox(height: 16),
                 TextFormField(
-                  controller: userNameController,
+                  controller: addressController,
                   decoration: const InputDecoration(
-                    labelText: 'Username',
-                    prefixIcon: Icon(Icons.person_outline),
+                    labelText: 'Address',
+                    prefixIcon: Icon(Icons.home),
                   ),
                   validator: (value) {
                     if (value!.isEmpty) {
-                      return 'Please enter your username';
-                    }
-                    return null;
-                  },
-                ),
-              ] else ...[
-                TextFormField(
-                  controller: orgNameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Name of Organization',
-                    prefixIcon: Icon(Icons.business),
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter the name of your organization';
+                      return 'Please enter your address';
                     }
                     return null;
                   },
                 ),
                 const SizedBox(height: 16),
-                GestureDetector(
-                  onTap: _pickImage,
-                  child: Container(
-                    height: 150,
-                    width: 150,
-                    color: Colors.grey[300],
-                    child: _image != null
-                        ? Image.file(_image!, fit: BoxFit.cover)
-                        : const Icon(Icons.camera_alt, color: Colors.grey),
+                TextFormField(
+                  controller: contactNumberController,
+                  decoration: const InputDecoration(
+                    labelText: 'Contact Number',
+                    prefixIcon: Icon(Icons.phone),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your contact number';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // String? error =
+                      //           await context.read<UserAuthProvider>().signUp(
+                      //                 userNameController.text.trim(),
+                      //                 passwordController.text.trim(),
+                      //               );
+                      //       if (error == null) {
+                      //         // Save additional user info to Firestore
+                      //         final user = context.read<UserAuthProvider>().user;
+                      //         await FirebaseFirestore.instance
+                      //             .collection('users')
+                      //             .doc(user?.uid)
+                      //             .set({
+                      //           'Name': nameController.text.trim(),
+                      //           'Username': userNameController.text.trim(),
+                      //           'Address': addresController.text.trim(),
+                      //           'Contact Number' : contactNumberController.text.trim(),
+                      //         });
+                      //         Navigator.pop(context);
+                      //       } else {
+                      //         setState(() {
+                      //           errorMessage = error;
+                      //         });
+                      //       }
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                // const SizedBox(height: 16),
-                // TextFormField(
-                //   controller: proofController,
-                //   decoration: const InputDecoration(
-                //     labelText: 'Proof of Legitimacy',
-                //     prefixIcon: Icon(Icons.verified),
-                //   ),
-                //   validator: (value) {
-                //     if (value!.isEmpty) {
-                //       return 'Please provide proof of legitimacy';
-                //     }
-                //     return null;
-                //   },
-                // ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: _resetForm,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                  ),
+                  child: const Text(
+                    'Reset',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ],
-              const SizedBox(height: 16),
-              TextFormField(
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  prefixIcon: Icon(Icons.lock),
-                ),
-                obscureText: true,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                controller: addressController,
-                decoration: const InputDecoration(
-                  labelText: 'Address',
-                  prefixIcon: Icon(Icons.home),
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your address';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                controller: contactNumberController,
-                decoration: const InputDecoration(
-                  labelText: 'Contact Number',
-                  prefixIcon: Icon(Icons.phone),
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your contact number';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // String? error =
-                    //           await context.read<UserAuthProvider>().signUp(
-                    //                 userNameController.text.trim(),
-                    //                 passwordController.text.trim(),
-                    //               );
-                    //       if (error == null) {
-                    //         // Save additional user info to Firestore
-                    //         final user = context.read<UserAuthProvider>().user;
-                    //         await FirebaseFirestore.instance
-                    //             .collection('users')
-                    //             .doc(user?.uid)
-                    //             .set({
-                    //           'Name': nameController.text.trim(),
-                    //           'Username': userNameController.text.trim(),
-                    //           'Address': addresController.text.trim(),
-                    //           'Contact Number' : contactNumberController.text.trim(),
-                    //         });
-                    //         Navigator.pop(context);
-                    //       } else {
-                    //         setState(() {
-                    //           errorMessage = error;
-                    //         });
-                    //       }
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                ),
-                child: const Text(
-                  'Sign Up',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 8),
-              ElevatedButton(
-                onPressed: _resetForm,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.grey,
-                ),
-                child: const Text(
-                  'Reset',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
