0d7f45eec9487b524fe2ac7fd9d9f3f220a07d8a
krispypatata <ksgabinete@up.edu.ph>
2024-05-17 11:44:25 +0800

    refactor: CHANGE builder of donor list from ListView to SliverList

--- lib/screens/donor_list_page.dart
+++ lib/screens/donor_list_page.dart
@@ -1,7 +1,7 @@
-import 'package:elbi_donation_system/components/rounded_image.dart';
 import 'package:flutter/material.dart';
 import '../models/route_model.dart';
 import '../models/user_model.dart';
+import '../components/rounded_image.dart';
 import '../dummy_data/dummy_users.dart';
 
 class DonorListPage extends StatefulWidget {
@@ -35,125 +35,144 @@ class _DonorListPageState extends State<DonorListPage> {
       .expand((list) => list)
       .toList();
 
-  // List<User> donors = List.generate(20, (index) {
-  //   return User(
-  //     id: "donor_$index",
-  //     name: "Donor ${index + 1}",
-  //     username: "donor${index + 1}_username",
-  //     email: "donor${index + 1}@example.com",
-  //     password: "password${index + 1}",
-  //     address: ["Address ${index + 1}"],
-  //     contactNo: "+123456789${index + 1}",
-  //     role: UserRole.donor.toString(),
-  //     profilePhoto:
-  //         "https://i.pinimg.com/originals/f5/24/e1/f524e1e728b829b039c84f5ee4f1478a.webp",
-  //   );
-  // });
-
   @override
   Widget build(BuildContext context) {
+    BorderRadius customBorder = const BorderRadius.only(
+      topLeft: Radius.circular(30),
+      topRight: Radius.circular(4),
+      bottomLeft: Radius.circular(4),
+      bottomRight: Radius.circular(30),
+    );
+
+    BorderRadius customBorderAlt = const BorderRadius.only(
+      topLeft: Radius.circular(4),
+      topRight: Radius.circular(30),
+      bottomLeft: Radius.circular(30),
+      bottomRight: Radius.circular(4),
+    );
+
     // build a widget that returns a list of donors
     Widget displayDonorList() {
-      BorderRadius customBorder = const BorderRadius.only(
-        topLeft: Radius.circular(30),
-        topRight: Radius.circular(4),
-        bottomLeft: Radius.circular(4),
-        bottomRight: Radius.circular(30),
-      );
-
-      BorderRadius customBorderAlt = const BorderRadius.only(
-        topLeft: Radius.circular(4),
-        topRight: Radius.circular(30),
-        bottomLeft: Radius.circular(30),
-        bottomRight: Radius.circular(4),
-      );
-
-      return ListView.builder(
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: donors.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding:
-                const EdgeInsets.only(left: 10, right: 10, top: 4, bottom: 10),
-            child: Container(
-              height: 100,
-              decoration: BoxDecoration(
-                borderRadius: (index % 2 == 0) ? customBorder : customBorderAlt,
-                color: Theme.of(context).colorScheme.surface, // white
-              ),
-              child: Card(
-                elevation: 0,
-                shape: RoundedRectangleBorder(
+      return SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.only(
+                  left: 10, right: 10, top: 4, bottom: 10),
+              child: Container(
+                height: 100,
+                decoration: BoxDecoration(
                   borderRadius:
                       (index % 2 == 0) ? customBorder : customBorderAlt,
+                  color: Theme.of(context).colorScheme.surface, // white
                 ),
-                child: InkWell(
-                  splashColor: Theme.of(context).colorScheme.primary,
-                  borderRadius:
-                      (index % 2 == 0) ? customBorder : customBorderAlt,
-                  onTap: () {/*for effects only*/},
-                  child: Center(
-                    child: ListTile(
-                      leading: RoundedImage(
-                        source: donors[index].profilePhoto!,
-                        size: 50,
-                      ),
-                      title: Text(donors[index].name),
-                      subtitle: Text(
-                        donors[index].username,
-                        style: const TextStyle(fontStyle: FontStyle.italic),
-                      ),
-                      trailing: ElevatedButton(
-                        style: ButtonStyle(
+                child: Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        (index % 2 == 0) ? customBorder : customBorderAlt,
+                  ),
+                  child: InkWell(
+                    splashColor: Theme.of(context).colorScheme.primary,
+                    borderRadius:
+                        (index % 2 == 0) ? customBorder : customBorderAlt,
+                    onTap: () {/*for effects only*/},
+                    child: Center(
+                      child: ListTile(
+                        leading: RoundedImage(
+                          source: donors[index].profilePhoto!,
+                          size: 50,
+                        ),
+                        title: Text(donors[index].name),
+                        subtitle: Text(
+                          donors[index].username,
+                          style: const TextStyle(fontStyle: FontStyle.italic),
+                        ),
+                        trailing: ElevatedButton(
+                          style: ButtonStyle(
                             elevation: WidgetStateProperty.all(0),
                             shape:
                                 WidgetStateProperty.all<RoundedRectangleBorder>(
                               RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20.0)),
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
                             ),
-                            overlayColor:
-                                WidgetStateProperty.resolveWith((states) {
-                              return states.contains(WidgetState.pressed)
-                                  ? Theme.of(context).cardColor
-                                  : null;
-                            })),
-                        onPressed: () {
-                          // Navigate to donor profile page
-                        },
-                        child: const Text('View Profile'),
+                            overlayColor: WidgetStateProperty.resolveWith(
+                              (states) {
+                                return states.contains(WidgetState.pressed)
+                                    ? Theme.of(context).cardColor
+                                    : null;
+                              },
+                            ),
+                          ),
+                          onPressed: () {
+                            // Navigate to donor's profile page
+                          },
+                          child: const Text('View Profile'),
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+          childCount: donors.length,
+        ),
       );
     }
 
     Widget displayHeader() {
       return Container(
         decoration: BoxDecoration(color: Theme.of(context).cardColor),
-        child: Row(
+        child: Column(
           children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 3,
-              height: 200,
-              child: const Center(
-                  child: Icon(
-                Icons.people_rounded,
-                size: 100,
-              )),
+            const SizedBox(height: 75),
+            Row(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 3,
+                  height: 125,
+                  child: const Center(
+                      child: Icon(
+                    Icons.people_rounded,
+                    size: 100,
+                  )),
+                ),
+                const Flexible(
+                  child: Text(
+                    "Manage Donors",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                )
+              ],
             ),
-            const Flexible(
-              child: Text(
-                "Manage Donors",
-                style: TextStyle(fontSize: 30),
-              ),
-            )
           ],
+        ),
+      );
+    }
+
+    displayAppBar() {
+      return SliverAppBar(
+        automaticallyImplyLeading: true,
+        elevation: 0,
+        pinned: true,
+        centerTitle: false,
+        expandedHeight: 200,
+        flexibleSpace: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            var top = constraints.biggest.height;
+            return FlexibleSpaceBar(
+              collapseMode: CollapseMode.parallax,
+              title: AnimatedOpacity(
+                duration: const Duration(milliseconds: 10),
+                opacity: top <= kToolbarHeight + 50 ? 1.0 : 0.0,
+                child: const Text("Donation List Page"),
+              ),
+              centerTitle: false,
+              background: displayHeader(),
+            );
+          },
         ),
       );
     }
@@ -171,24 +190,19 @@ class _DonorListPageState extends State<DonorListPage> {
       );
     }
 
-    // build a widget that returns a column of widgets in singlescrollview
-    Widget buildSingleChildScrollView() {
-      return SingleChildScrollView(
-        child: Column(
-          children: [
-            displayHeader(),
-            const SizedBox(height: 20),
-            displayDonorList(),
-            displayFooter(),
-          ],
-        ),
-      );
-    }
-
     return Scaffold(
-      appBar: AppBar(title: const Text('Donation List Page')),
-      body: Container(
-        child: buildSingleChildScrollView(),
+      // appBar: AppBar(title: const Text('Donation List Page')),
+      body: CustomScrollView(
+        slivers: [
+          displayAppBar(),
+          const SliverToBoxAdapter(
+            child: SizedBox(height: 10),
+          ),
+          displayDonorList(),
+          SliverToBoxAdapter(
+            child: displayFooter(),
+          ),
+        ],
       ),
     );
   }
