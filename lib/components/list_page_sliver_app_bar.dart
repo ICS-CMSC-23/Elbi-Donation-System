6365f357faf5f219c77e6fd24658b2649491942f
krispypatata <ksgabinete@up.edu.ph>
2024-05-18 01:52:05 +0800

    feat: ADD builder component for a list page sliver appbar

---
+++ lib/components/list_page_sliver_app_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ListPageSliverAppBar extends StatelessWidget {
+  final String title;
+  final Widget backgroundWidget;
+  const ListPageSliverAppBar({
+    super.key,
+    required this.title,
+    required this.backgroundWidget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      automaticallyImplyLeading: true,
+      elevation: 0,
+      pinned: true,
+      centerTitle: false,
+      expandedHeight: 200,
+      stretch: true,
+      flexibleSpace: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          var top = constraints.biggest.height;
+          return FlexibleSpaceBar(
+            title: AnimatedOpacity(
+              duration: const Duration(milliseconds: 10),
+              opacity: top <= kToolbarHeight + 50 ? 1.0 : 0.0,
+              child: Text(title),
+            ),
+            centerTitle: false,
+            background: backgroundWidget,
+          );
+        },
+      ),
+    );
+  }
+}
