ef29ed50406019b2364e0f32316da0e7c0e36e1b
krispypatata <ksgabinete@up.edu.ph>
2024-05-17 22:25:29 +0800

    chore: UPDATE design for donation drive list

--- lib/screens/list_pages/donation_drive_list_page.dart
+++ lib/screens/list_pages/donation_drive_list_page.dart
@@ -63,58 +63,128 @@ class _DonationDriveListPageState extends State<DonationDriveListPage> {
                     borderRadius:
                         (index % 2 == 0) ? customBorder : customBorderAlt,
                     onTap: () {/*for effects only*/},
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // donation drive list images
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: SingleChildScrollView(
-                                scrollDirection: Axis.horizontal,
-                                child: Row(
-                                  children: [
-                                    for (int i = 0;
-                                        i <
-                                            donationDrives[index]
-                                                .photos!
-                                                .length;
-                                        i++)
-                                      Padding(
-                                        padding: i <
-                                                donationDrives[index]
-                                                        .photos!
-                                                        .length -
-                                                    1
-                                            ? const EdgeInsets.only(right: 8.0)
-                                            : EdgeInsets.zero,
-                                        child: Container(
-                                          height: 100,
-                                          width: 200,
-                                          decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(10),
-                                            image: DecorationImage(
-                                              image: Image.network(
-                                                      donationDrives[index]
-                                                          .photos![i])
-                                                  .image,
-                                              fit: BoxFit.cover,
+                    child: Stack(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              top: 15, left: 20, right: 20, bottom: 15),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(bottom: 5),
+                                  child: Text(
+                                    donationDrives[index].name,
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              // donation drive list images
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: SingleChildScrollView(
+                                  scrollDirection: Axis.horizontal,
+                                  child: Row(
+                                    children: [
+                                      for (int i = 0;
+                                          i <
+                                              donationDrives[index]
+                                                  .photos!
+                                                  .length;
+                                          i++)
+                                        Padding(
+                                          padding: i <
+                                                  donationDrives[index]
+                                                          .photos!
+                                                          .length -
+                                                      1
+                                              ? const EdgeInsets.only(
+                                                  right: 8.0)
+                                              : EdgeInsets.zero,
+                                          child: Container(
+                                            height: 100,
+                                            width: 200,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                              image: DecorationImage(
+                                                image: Image.network(
+                                                        donationDrives[index]
+                                                            .photos![i])
+                                                    .image,
+                                                fit: BoxFit.cover,
+                                              ),
                                             ),
                                           ),
                                         ),
-                                      ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                            Text("Title: ${donationDrives[index].name}"),
-                          ],
+
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 7, left: 5, right: 5, bottom: 0),
+                                child: RichText(
+                                  textAlign: TextAlign.justify,
+                                  text: TextSpan(
+                                    style: TextStyle(
+                                      // fontSize: 14.0,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onSurface,
+                                      height: 1.5,
+                                      letterSpacing: 1.1,
+                                    ),
+                                    children: <TextSpan>[
+                                      const TextSpan(
+                                        text: 'Start Date:\t',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text:
+                                            '${donationDrives[index].startDate.toString().substring(0, 10)}\n',
+                                      ),
+                                      const TextSpan(
+                                        text: 'End Date:\t\t\t',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: donationDrives[index]
+                                            .endDate
+                                            .toString()
+                                            .substring(0, 10),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
+                        Positioned(
+                          bottom: 10,
+                          right: 10,
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              elevation: WidgetStateProperty.all(0),
+                            ),
+                            onPressed: () {
+                              // Implement view full details functionality
+                            },
+                            child: const Text('View Full Details'),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
@@ -139,13 +209,13 @@ class _DonationDriveListPageState extends State<DonationDriveListPage> {
                   height: 125,
                   child: const Center(
                       child: Icon(
-                    Icons.people_rounded,
+                    Icons.real_estate_agent_rounded,
                     size: 100,
                   )),
                 ),
                 const Flexible(
                   child: Text(
-                    "Manage Donors",
+                    "Donation Drives",
                     style: TextStyle(fontSize: 30),
                   ),
                 )
@@ -207,7 +277,7 @@ class _DonationDriveListPageState extends State<DonationDriveListPage> {
           const SliverToBoxAdapter(
             child: SizedBox(height: 10),
           ),
-          // displayDonationDriveList(),
+          displayDonationDriveList(),
           displayFooter(),
         ],
       ),
