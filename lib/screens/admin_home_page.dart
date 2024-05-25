d42a0e5802a5153e4d7290f2a9edc8042d807ed1
Gab D <gddiocadiz@up.edu.ph>
2024-05-25 22:13:48 +0800

    feat: integrated firebase to admin home page

--- lib/screens/admin_home_page.dart
+++ lib/screens/admin_home_page.dart
@@ -1,13 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:elbi_donation_system/components/main_drawer.dart';
-import 'package:elbi_donation_system/dummy_data/dummy_users.dart';
 import 'package:elbi_donation_system/models/route_model.dart';
 import 'package:elbi_donation_system/models/user_model.dart';
-import 'package:elbi_donation_system/providers/user_list_provider.dart';
 import 'package:elbi_donation_system/providers/user_provider.dart';
 import 'package:elbi_donation_system/screens/donor_list_page.dart';
-import 'package:elbi_donation_system/screens/org_profile_page.dart';
 import 'package:flutter/material.dart';
-import 'package:elbi_donation_system/dummy_data/dummy_orgs.dart';
 import 'package:provider/provider.dart';
 
 class AdminHomePage extends StatefulWidget {
@@ -20,43 +17,69 @@ class AdminHomePage extends StatefulWidget {
 class _AdminHomePageState extends State<AdminHomePage> {
   @override
   Widget build(BuildContext context) {
-    List<User> organizations =
-        dummyUsers.where((user) => user.role == User.organization).toList();
+    // Fetching the list of organizations from Firestore
+    Stream<QuerySnapshot> orgStream = FirebaseFirestore.instance.collection('users')
+        .where('role', isEqualTo: 'organization')
+        .snapshots();
 
     return Scaffold(
       drawer: MainDrawer(
         routes: [
           RouteModel("Logout", "/login"),
           RouteModel("Organization Account Approval", "/org-account-approval"),
-          DonorListPage.route
+          DonorListPage.route,
         ],
       ),
       appBar: AppBar(
         title: const Text("Admin Home Page"),
       ),
-      body: ListView.builder(
-        itemCount: organizations.length,
-        itemBuilder: (context, index) {
-          final organization = organizations[index];
-          return Card(
-            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage(organization.profilePhoto!),
-              ),
-              title: Text(organization.name),
-              subtitle: Text(
-                  organization.about ?? "This organization has no description"),
-              trailing: ElevatedButton(
-                onPressed: () {
-                  // Link Org Profile page
-                  context.read<UserProvider>().changeSelectedUser(organization);
+      body: StreamBuilder<QuerySnapshot>(
+        stream: orgStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Error: ${snapshot.error}"),
+            );
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(
+              child: Text("No organizations found."),
+            );
+          }
 
-                  Navigator.pushNamed(context, "/org-profile");
-                },
-                child: const Text("View Org"),
-              ),
-            ),
+          var organizations = snapshot.data!.docs.map((doc) {
+            return User.fromJson(doc.data() as Map<String, dynamic>);
+          }).toList();
+
+          return ListView.builder(
+            itemCount: organizations.length,
+            itemBuilder: (context, index) {
+              final organization = organizations[index];
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(organization.profilePhoto ?? 'https://via.placeholder.com/150'),
+                  ),
+                  title: Text(organization.name),
+                  subtitle: Text(
+                      organization.about ?? "This organization has no description"),
+                  trailing: ElevatedButton(
+                    onPressed: () {
+                      // Link Org Profile page
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        context.read<UserProvider>().changeSelectedUser(organization);
+                        Navigator.pushNamed(context, "/org-profile");
+                      });
+                    },
+                    child: const Text("View Org"),
+                  ),
+                ),
+              );
+            },
           );
         },
       ),
