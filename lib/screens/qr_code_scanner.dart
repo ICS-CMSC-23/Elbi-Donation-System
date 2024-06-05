cee5455daa87e3b25d98a1252ca32a2eea4cce1b
krispypatata <ksgabinete@up.edu.ph>
2024-06-05 19:12:36 +0800

    feat: ADD initial setup file for qr code scanner

---
+++ lib/screens/qr_code_scanner.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class QrCodeScanner extends StatefulWidget {
+  const QrCodeScanner({super.key});
+
+  @override
+  State<QrCodeScanner> createState() => _QrCodeScannerState();
+}
+
+class _QrCodeScannerState extends State<QrCodeScanner> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
