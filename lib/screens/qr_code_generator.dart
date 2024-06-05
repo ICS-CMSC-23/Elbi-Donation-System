a55d92f8da7797e18333b78d935eb8a661bf1b88
krispypatata <ksgabinete@up.edu.ph>
2024-06-05 19:17:15 +0800

    chore: ADD route models

--- lib/screens/qr_code_generator.dart
+++ lib/screens/qr_code_generator.dart
@@ -1,8 +1,15 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import '../models/route_model.dart';
 
 class QrCodeGenerator extends StatefulWidget {
   const QrCodeGenerator({super.key});
+
+  // class route model
+  static final RouteModel _qrCodeGenerator = RouteModel(
+    "QR Code Generator",
+    "/qr-code-generator",
+  );
+  static RouteModel get route => _qrCodeGenerator;
 
   @override
   State<QrCodeGenerator> createState() => _QrCodeGeneratorState();
--- lib/screens/qr_code_scanner.dart
+++ lib/screens/qr_code_scanner.dart
@@ -1,8 +1,15 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import '../models/route_model.dart';
 
 class QrCodeScanner extends StatefulWidget {
   const QrCodeScanner({super.key});
+
+  // class route model
+  static final RouteModel _qrCodeScanner = RouteModel(
+    "QR Code Scanner",
+    "/qr-code-scanner",
+  );
+  static RouteModel get route => _qrCodeScanner;
 
   @override
   State<QrCodeScanner> createState() => _QrCodeScannerState();
