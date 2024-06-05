2a5c15fbe1ec52aa4bd8aa06278b6fc840b4aec8
krispypatata <ksgabinete@up.edu.ph>
2024-06-06 00:00:24 +0800

    chore: COMPLETE implementation of scanning qr code

--- lib/screens/qr_code_scanner.dart
+++ lib/screens/qr_code_scanner.dart
@@ -1,8 +1,15 @@
+import '../providers/donation_provider.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import '../models/route_model.dart';
+import '../models/donation_model.dart';
+import '../api/firebase_donation_api.dart';
+import '../screens/donation_details_page.dart';
 
 class QrCodeScanner extends StatefulWidget {
-  const QrCodeScanner({super.key});
+  const QrCodeScanner({Key? key}) : super(key: key);
 
   // class route model
   static final RouteModel _qrCodeScanner = RouteModel(
@@ -18,46 +25,76 @@ class QrCodeScanner extends StatefulWidget {
 class _QrCodeScannerState extends State<QrCodeScanner> {
   String qrCodeResult = "";
 
-  // Future<void> scanQrCode() async {
-  //   try {
-  //     final qrCode = await FlutterBarcodeScanner.scanBarcode(
-  //       "#ff6666",
-  //       "Cancel",
-  //       true,
-  //       ScanMode.QR,
-  //     );
+  @override
+  void initState() {
+    super.initState();
+    // call scanQrCode as soon as the widget is initialized
+    WidgetsBinding.instance!.addPostFrameCallback((_) => scanQrCode());
+  }
 
-  //     if (!mounted) return;
+  Future<void> scanQrCode() async {
+    try {
+      final qrCode = await FlutterBarcodeScanner.scanBarcode(
+        "#ff6666",
+        "Cancel",
+        true,
+        ScanMode.QR,
+      );
 
-  //     setState(() {
-  //       qrCodeResult = qrCode.toString();
-  //     });
+      if (!mounted) return;
 
-  //   } on PlatformException {
-  //     qrCodeResult = "Failed to get the QR Code";
-  //   }
-  // }
+      setState(() {
+        qrCodeResult = qrCode.toString();
+      });
+
+      // get the selected donation
+      Donation selectedDonation = context.read<DonationProvider>().selected;
+
+      // define SnackBar content
+      String snackBarMessage;
+      Color snackBarColor;
+
+      // check if the scanned QR code matches the donation ID
+      if (qrCodeResult == selectedDonation.id) {
+        // UD Matched
+        // update the status of the donation to "Completed"
+        selectedDonation.status = "Completed";
+        // update the donation in Firebase
+        await FirebaseDonationAPI().updateDonation(
+          selectedDonation.id!,
+          selectedDonation.toJson(),
+        );
+        snackBarMessage = 'Updated Donation Status Successfully';
+        snackBarColor = Colors.green[900]!;
+      } else {
+        // ID does not match
+        snackBarMessage = 'QR code does not match Donation ID';
+        snackBarColor = Colors.red[900]!;
+      }
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: snackBarColor,
+          content: Center(
+            child: Text(
+              snackBarMessage,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+          duration: const Duration(milliseconds: 1000),
+          behavior: SnackBarBehavior.fixed,
+        ),
+      );
+
+      // navigate back after scanning
+      Navigator.pop(context, qrCodeResult);
+    } on PlatformException {
+      qrCodeResult = "Failed to get the QR Code";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("QR Code Scanner"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 30),
-            Text("Result: $qrCodeResult"),
-            const SizedBox(height: 30),
-            // ElevatedButton(
-            //   // onPressed: () => scanQrCode(),
-            //   child: const Text("Scan QR Code"),
-            // ),
-          ],
-        ),
-      ),
-    );
+    return Container();
   }
 }
