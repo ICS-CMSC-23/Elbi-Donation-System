4922d77e68bf8e28391067e38fac4ee291265daa
krispypatata <ksgabinete@up.edu.ph>
2024-06-05 19:12:53 +0800

    feat: ADD initial setup file for qr code generator

---
+++ lib/screens/qr_code_generator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class QrCodeGenerator extends StatefulWidget {
+  const QrCodeGenerator({super.key});
+
+  @override
+  State<QrCodeGenerator> createState() => _QrCodeGeneratorState();
+}
+
+class _QrCodeGeneratorState extends State<QrCodeGenerator> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
