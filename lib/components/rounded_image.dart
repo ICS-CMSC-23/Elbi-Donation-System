52863935f0b2956c7be4c61a9a09f53be0647fb9
JomarMonreal <jpmonreal@up.edu.ph>
2024-06-05 15:56:06 +0800

    chore: rounded image can now be viewed

--- lib/components/rounded_image.dart
+++ lib/components/rounded_image.dart
@@ -36,9 +36,72 @@ class RoundedImage extends StatelessWidget {
       },
     );
 
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(size / 2),
-      child: imageType,
+    return GestureDetector(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(size / 2),
+        child: imageType,
+      ),
+      onTap: () async {
+        await showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              content: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Image.network(
+                      source,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        try {
+                          return Image.memory(
+                            decodeBase64Image(source),
+                            errorBuilder: (context, error, stackTrace) {
+                              return ClipRRect(
+                                borderRadius: BorderRadius.circular(size / 2),
+                                child: Image.asset(
+                                    'assets/images/portrait-placeholder.jpg',
+                                    fit: BoxFit.cover),
+                              );
+                            },
+                            fit: BoxFit.cover,
+                          );
+                        } catch (e) {
+                          try {
+                            return Image.memory(
+                              decodeBase64ImageUncompressed(source),
+                              errorBuilder: (context, error, stackTrace) {
+                                return ClipRRect(
+                                  borderRadius: BorderRadius.circular(size / 2),
+                                  child: Image.asset(
+                                      'assets/images/portrait-placeholder.jpg',
+                                      fit: BoxFit.cover),
+                                );
+                              },
+                              fit: BoxFit.cover,
+                            );
+                          } catch (e) {
+                            return Image.asset(
+                                'assets/images/portrait-placeholder.jpg',
+                                fit: BoxFit.cover);
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('Exit'),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
